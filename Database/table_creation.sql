03e91a6bf0af5c9a4ce08eb3d4ea583a99a4b09d
manahilA <88901268+manahilA@users.noreply.github.com>
2022-04-15 18:40:55 -0400

    Update table_creation.sql

--- Database/table_creation.sql
+++ Database/table_creation.sql
@@ -16,6 +16,8 @@ CREATE TABLE contacts(
 
 CREATE TABLE users(
 	username VARCHAR(100) PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR (50),
 	password VARCHAR(255) NOT NULL,
 	role_type ENUM('super admin', 'admin', 'student') DEFAULT 'student',
 	email VARCHAR(100) NOT NULL,
